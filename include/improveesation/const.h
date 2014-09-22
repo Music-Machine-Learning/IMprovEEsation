7040fc24fe1c01ddcc74ead92dc75f0b348c6c8a
matteomartelli <matteomartelli3@gmail.com>
2014-09-22 17:57:16 +0200

    update rest midi note constant

--- include/improveesation/const.h
+++ include/improveesation/const.h
@@ -107,7 +107,7 @@
 #define MIDI_NOCTAVES  8 /* From A0 to C8 */
 #define MIDI_FIRST_NOTE 21 /* A0 */
 #define MIDI_LAST_NOTE 108 /* C8 */
-#define MIDI_REST_NOTE	0
+#define MIDI_REST_NOTE	128
 
 #define NO_COUPLING 0
 
