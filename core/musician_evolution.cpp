012684117978e9d2d75a8a0a2ccf23b91bcbf3cf
stradivarius <fede.selmer@gmail.com>
2014-10-15 12:12:48 +0200

    evolution prototype

---
+++ core/musician_evolution.cpp
@@ -0,0 +1,166 @@
+/*****************************************************************************/
+/* Genetic loop to evolve an improvised melody                               */
+/* This program is a part of the IMprovEEsation suite.                       */
+/*                                                                           */
+/* Copyright (C) 2014                                                        */
+/* Federico Montori, Marco Melletti, Davide Berardi, Matteo Martelli         */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or any later version.                                     */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,*/
+/* USA.                                                                      */
+/*****************************************************************************/
+
+	#include <stdio.h>
+	#include <improveesation/communication.h>
+	#include <improveesation/structs.h>
+
+	#include <improveesation/musician_evolution.h>
+
+	#include <unistd.h>
+	#include <stdlib.h>
+	#include <signal.h>
+	#include <string.h>
+
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <time.h>
+
+void swap_pieces(struct piece_s * fst, struct piece_s * snd){
+	struct piece_s temp;
+	temp = *fst;
+	*fst = *snd;
+	*snd = temp;
+	}
+
+/* Function that checks how much the array trial is similar to goal and stores the similarity in the 3array */
+int compute_similarity(struct piece_s * ggoal, struct piece_s * gtrial, uint8_t * sim){
+	
+	int i,k;
+	int csim = 0;
+	int ctot = 0;
+	
+	/* FIXME check the effective array length */
+	for(i=0; i<ggoal->count; i++){
+		
+		if (ggoal->notes[i].notes[0] == gtrial->notes[i].notes[0])
+			csim++;
+		if (ggoal->notes[i].tempo == gtrial->notes[i].tempo)
+			csim++;
+		if (ggoal->notes[i].triplets == gtrial->notes[i].triplets)
+			csim++;
+		ctot += 3;
+	}
+	
+	*sim = ((csim * 100) / ctot);
+	return ((csim * 100) / ctot);
+}
+
+/* Functions which sort the pool according to similarity (with mergesort) */
+void merge_pool(struct piece_s * pool [GENETIC_POOL_SIZE], uint8_t * sim, int fst, int q, int end){
+	int lw, k, hh, j, B[GENETIC_POOL_SIZE];
+	struct piece_s sandbox[GENETIC_POOL_SIZE];
+	lw = fst; hh = q + 1; k = fst;	
+	while(lw <= q && hh <= end){
+		if(sim[lw] <= sim[hh]){
+			B[k] = sim[lw];
+			sandbox[k] = *pool[lw];
+			lw++;
+		} else {
+			B[k] = sim[hh];
+			sandbox[k] = *pool[hh];
+			hh++;
+		}
+		k++;
+	}
+	if (lw > q)
+		for(j = hh; j <= end; j++){
+			B[k] = sim[j];
+			sandbox[k] = *pool[j];
+			k++;
+		 } 
+	else
+		for(j = lw; j <= q; j++){
+			B[k] = sim[j];
+			sandbox[k] = *pool[j];
+			k++;
+		}	
+	for (j = fst; j <= end; j++)
+		sim[j] = B[j];
+		*pool[j] = sandbox[j];
+}
+
+void mergesort_pool(struct piece_s * pool[GENETIC_POOL_SIZE], uint8_t * sim, int fst, int end){
+	int q;
+	if (fst < end){
+		q = ((fst + end) / 2);
+		mergesort_pool(pool, sim, fst, q);
+		mergesort_pool(pool, sim, q + 1 ,end);
+		merge_pool(pool, sim, fst, q, end);
+	}
+}
+
+int sort_pool(struct piece_s * pool[GENETIC_POOL_SIZE], uint8_t * sim){
+	mergesort_pool(pool, sim, 0, GENETIC_POOL_SIZE - 1);
+	return 0;
+}
+
+int genetic_loop(struct piece_s * ginitial, struct piece_s * ggoal){
+	
+	/* Arrays that contain the genetic pool generated every round */
+	struct piece_s genetic_pool[GENETIC_POOL_SIZE];
+	/* Array that stats the similarity coefficient (0-100) with the ideal, the postion of the most similar and the position of the less similar */
+	uint8_t similarity[GENETIC_POOL_SIZE][3];
+	/* Array that stats the similarity coefficient (0-100) with the ideal */
+	uint8_t sim[GENETIC_POOL_SIZE];
+	
+	int i,j;
+	
+	/* Initialize the genetic pool with copies of the original (and the similarities with 0 */
+	for(i=0; i<GENETIC_POOL_SIZE; i++){
+		for(j=0; j<ginitial->count; j++){
+			genetic_pool[i].notes[j] = ginitial->notes[j];
+		}
+		
+		/* FIXME to remove when the similarity is complete */
+		similarity[i][0] = 0;
+		similarity[i][1] = 0;
+		similarity[i][2] = 0;
+		
+		compute_similarity(ggoal, &genetic_pool[i], &sim[i]);	
+	}
+	
+	
+	
+	/* Now we have everything set, let's get started with the loop! */
+	for(i=0; i<GENETIC_ROUNDS; i++){
+		
+		/* Recombination and transposon propagation (skip on the first iteration) */
+		
+		/* First we change a number (according to the length) of notes at random */
+		
+		/* Then we need to compute similarity and sort the array according to it */
+		for(i=0; i<GENETIC_POOL_SIZE; i++)
+			compute_similarity(ggoal, &genetic_pool[i], &sim[i]);
+		
+		sort_pool(&genetic_pool, sim);
+
+	}
+	
+	/* Need to sort once again */
+	
+	
+
+
+	return 0;
+}
---
+++ include/improveesation/musician_evolution.h
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* Genetic loop to evolve an improvised melody                               */
+/* This program is a part of the IMprovEEsation suite.                       */
+/*                                                                           */
+/* Copyright (C) 2014                                                        */
+/* Federico Montori, Marco Melletti, Davide Berardi, Matteo Martelli         */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or any later version.                                     */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,*/
+/* USA.                                                                      */
+/*****************************************************************************/
+
+#ifndef MUSICIAN_EVOLUTION_H
+#define MUSICIAN_EVOLUTION_H
+
+#define MEASURE_NUM 32
+#define MELODY_LENGTH (MEASURE_NUM * 24) /* FIXME MEASURE_NUM is temporary, it shouldn't be hardcoded */
+
+int genetic_loop(uint8_t ** initial[], uint8_t ** ideal[]);
+
+#endif
